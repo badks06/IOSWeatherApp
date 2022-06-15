840e93c8c9b37bbcb73fb22582602a2764a87fe9
Benoit Chirazi <bchirazi@gmail.com>
2022-06-15 15:30:01 +0200

    add files

---
+++ WeatherApp/Components/WeatherRow.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherRow.swift
+//  WeatherApp
+//
+//  Created by Adk Soft on 09/03/2022.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var logo: String
+    var name: String
+    var value: String
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(systemName: logo)
+                .font(.title2)
+                .frame(width: 20, height: 20)
+                .padding()
+                .background(Color(hue: 0.64, saturation: 0.706, brightness: 0.693))
+                .cornerRadius(50)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(name)
+                    .font(.caption)
+                
+                Text(value)
+                    .bold()
+                    .font(.title)
+            }
+            
+        }
+    }
+}
+
+struct WeatherRow_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherRow(logo: "thermometer", name: "Feels like", value: "8")
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Adk Soft on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
